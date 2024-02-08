b74ec3b743574af6b4fd93ddf718d664a368fcfd
sona-flutter <fluttersonali07@gmail.com>
2024-02-08 13:39:09 +0530

    complete

---
+++ Project/Stateful/flutter_application_2/lib/file1.dart
@@ -0,0 +1,118 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Portfolio",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.grey,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        backgroundColor: Colors.blue.shade200,
+        child: const Text(
+          "Next",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.grey,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                (counter >= 1)
+                    ? const Text(
+                        "Name: Sonali Dhavale",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    : Container(),
+                const SizedBox(
+                  height: 20,
+                ),
+                (counter >= 2)
+                    ? SizedBox(
+                        child: Image.network(
+                          "https://tse3.mm.bing.net/th?id=OIP.bv3Kaa9kn87PXzwEU6WKigHaHa&pid=Api&P=0&h=180",
+                          height: 150,
+                        ),
+                        // alignment: Alignment.topLeft,
+                        // height: double.infinity,
+                      )
+                    : Container(),
+                const SizedBox(
+                  height: 20,
+                ),
+                (counter >= 3)
+                    ? const Text(
+                        "College Name: SSPU",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    : Container(),
+                const SizedBox(
+                  height: 20,
+                ),
+                (counter >= 4)
+                    ? Image.network(
+                        "https://1.bp.blogspot.com/-M9X1wtKsT2k/X2dtJMWF6tI/AAAAAAAAAJ4/PpvqfzInPbAsSN3zmo6rXu6rC5r1QMY9QCLcBGAsYHQ/s2048/University_of_Pune_-Main_Building_1.jpg",
+                        height: 150,
+                        width: 200,
+                      )
+                    : Container(),
+                const SizedBox(
+                  height: 20,
+                ),
+                (counter >= 5)
+                    ? const Text(
+                        "Dream Company:  Google",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    : Container(),
+                const SizedBox(
+                  height: 20,
+                ),
+                (counter >= 6)
+                    ? Image.network(
+                        "https://tse1.mm.bing.net/th?id=OIP.BZflXgPLOzJ2uDtMyyE5hwHaDF&pid=Api&P=0&h=180",
+                        height: 100,
+                      )
+                    : Container(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
